ce281f3d1464459297568720943dae15aebd12c7
NataliaFrancisca <nathifrancisca@gmail.com>
2022-07-22 11:31:19 -0300

    style: change board name and button color

--- src/styles/elements.jsx
+++ src/styles/elements.jsx
@@ -1,31 +1,37 @@
 import styled from "styled-components";
 
 export const ButtonCounter = styled.button`
-    background-color: #B2A4FF;
+    background-color: rgba(240,240,240,.7);
     border-radius: 4px;
 
-    padding: 10px;
+    padding: 2px;
 
-    font-size: 5em;
-    min-width: 100px;
-    width: auto;
+    font-size: 1em;
+    width: 90%;
 
     cursor: pointer;
 `
 
-export const DisplayScore = styled.main`
-    background-color: #FFB4B4;
-    
-    border-radius: 4px;
+export const Board = styled.main`
+    width: 60%;
     padding: 20px;
+    border: solid black 6px;
+    border-radius: 4px;
+
+    background-color: #B2A4FF;
 
     display: flex;
     align-items: center;
-    justify-content: center;
+    justify-content: space-around;
 
 
-    & h1{
-        font-size: 5em;
-        margin: 0 50px;
+    & span{
+        font-size: 2em;
+        font-weight: 600;
+
+        padding: 10px 50px;
+        border-radius: 4px;
+
+        background-color: rgba(240,240,240,.6);
     }
 `
