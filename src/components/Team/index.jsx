96b993e18fcdc33124992d09597a6bf7aa4ad694
NataliaFrancisca <nathifrancisca@gmail.com>
2022-07-25 10:59:10 -0300

    feat: create team component

--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -1,12 +1,27 @@
-import { Score } from "../Score/style";
+import { useEffect, useRef } from "react";
+import { ButtonCounter } from "../../styles/elements";
 
-const TeamComponent = ({nameTeam, className}) => {
+const TeamComponent = ({nameTeam, className, counterTeam, onDecrement, onIncrement}) => {
+
+    const btnDecrementRef = useRef();
+
+    useEffect(() => {
+        counterTeam == 0 ? btnDecrementRef.current.disabled = true : btnDecrementRef.current.disabled = false;
+    })
+
     return(
         <section className={className}>
             <h2 className="title-team">{nameTeam}</h2>
-            <Score />
+            <div className="container-points">
+                <ButtonCounter onClick={onIncrement}>+1</ButtonCounter>
+                    <h1>{counterTeam}</h1>
+                <ButtonCounter onClick={onDecrement} ref={btnDecrementRef}>-1</ButtonCounter> 
+            </div>
         </section>
     )
 }
 
 export default TeamComponent;
+
+
+
