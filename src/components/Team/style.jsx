8974884d085f532e7aba16e7499f78da129ffbca
NataliaFrancisca <nathifrancisca@gmail.com>
2022-07-25 12:30:09 -0300

    feat: add button decrement and change container

--- src/components/Team/style.jsx
+++ src/components/Team/style.jsx
@@ -8,6 +8,7 @@ export const TeamBoard = styled(TeamComponent)`
     justify-content: space-between;
 
     min-width: 40%;
+    position: relative;
 
     .title-team{
         font-size: 1.6em;
@@ -24,7 +25,7 @@ export const TeamBoard = styled(TeamComponent)`
         background-color: #FFB4B4;
     
         border-radius: 4px;
-        padding: 30px;
+        padding: 40px;
         margin-top: 20px;
         box-shadow: 0px 0px 10px -1px rgba(20,20,20,.5);
 
@@ -33,7 +34,14 @@ export const TeamBoard = styled(TeamComponent)`
         flex-direction: column;
         justify-content: center;
 
-        width: 50%;
+        width: 60%;
+        cursor: pointer;
+
+        font-size: 5em;
+        font-weight: 600;
+        font-family: 'Orbitron', sans-serif;
+
+        position: relative;
 
         @media screen and (max-width: 900px){
             width: 80%;
@@ -42,10 +50,21 @@ export const TeamBoard = styled(TeamComponent)`
         @media screen and (max-width: 550px){
             width: 90%;
         }
-
-        & h1{
-            font-size: 5em;
-            font-family: 'Orbitron', sans-serif;
-        }
     }
+
+`
+export const ButtonDecrement = styled.button`
+    background-color: rgba(249,76,102,.7);
+    border-radius: 4px;
+
+    padding: 10px;
+    width: 60%;
+
+    cursor: pointer;
+    font-size: 1.5em;
+    font-weight: 600;
+
+    z-index: 11;
+
+    margin-top: 10px;
 `
