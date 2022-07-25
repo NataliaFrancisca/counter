fddef3ce4823961315932ff270582dab667c7ef8
NataliaFrancisca <nathifrancisca@gmail.com>
2022-07-25 11:00:09 -0300

    style: change elements styles

--- src/styles/elements.jsx
+++ src/styles/elements.jsx
@@ -12,24 +12,47 @@ export const ButtonCounter = styled.button`
     cursor: pointer;
 `
 
+export const ButtonClean = styled.button`
+    background-color: #EB4747;
+    padding: 5px;
+
+    width: 90%;
+
+    cursor: pointer;
+`
+
 export const Board = styled.main`
-    width: 60%;
-    padding: 20px;
+    min-width: 60%;
+    max-width: 90%;
+    width: auto;
+
+    padding: 30px 20px;
     border: solid black 6px;
     border-radius: 4px;
 
     background-color: #B2A4FF;
 
-    display: flex;
+    display: grid;
+    grid-template-columns: 2fr 1fr 2fr;
     align-items: center;
-    justify-content: space-around;
 
+    @media screen and (max-width: 600px)  {
+        transform: rotate(-90deg);
+        overflow-x: hidden;
+        min-width: 90vh;
+    }
+
+    @media screen and (max-width: 760px){
+        grid-template-columns: 3fr 1fr 3fr;
+    }
 
     & span{
+        height: fit-content;
+        text-align: center;
         font-size: 2em;
         font-weight: 600;
 
-        padding: 10px 50px;
+        padding: 10px;
         border-radius: 4px;
 
         background-color: rgba(240,240,240,.6);
