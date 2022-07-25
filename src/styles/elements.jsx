7299d6d62d95b0a827c2625383a601a93a121428
NataliaFrancisca <nathifrancisca@gmail.com>
2022-07-25 15:32:10 -0300

    style: add background mask

--- src/styles/elements.jsx
+++ src/styles/elements.jsx
@@ -32,36 +32,10 @@ export const Button = styled.button`
     }
 `
 
-export const Board = styled.main`
-    min-width: 50%;
-    max-width: 90%;
-    width: auto;
-
-    padding: 20px;
-    border: solid black 6px;
-    border-radius: 4px;
-
-    background-color: #B2A4FF;
-
-    display: grid;
-    grid-template-columns: 2fr 1fr 2fr;
-    align-items: center;
-
-    position: relative;
-
-    @media screen and (max-width: 760px){
-        grid-template-columns: 3fr 1fr 3fr;
-    }
-
-    & span{
-        height: fit-content;
-        text-align: center;
-        font-size: 2em;
-        font-weight: 600;
-
-        padding: 10px;
-        border-radius: 4px;
-
-        background-color: rgba(240,240,240,.6);
-    }
+export const BackgroundMask = styled.div`
+    position: fixed;
+    background-color: rgba(0, 0, 0, 0.5);
+    -webkit-tap-highlight-color: transparent;
+    z-index: 1;
+    inset: ${props => props.filter ? "0px" : 'none'};
 `
