21e1d59b44b5e58addf2f654148a148cde577502
NataliaFrancisca <nathifrancisca@gmail.com>
2022-07-25 12:29:45 -0300

    feat: change style button

--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from "react";
-import { ButtonCounter } from "../../styles/elements";
+
+import { ButtonDecrement } from "./style";
 
 const TeamComponent = ({nameTeam, className, counterTeam, onDecrement, onIncrement}) => {
 
@@ -12,11 +13,10 @@ const TeamComponent = ({nameTeam, className, counterTeam, onDecrement, onIncreme
     return(
         <section className={className}>
             <h2 className="title-team">{nameTeam}</h2>
-            <div className="container-points">
-                <ButtonCounter onClick={onIncrement}>+1</ButtonCounter>
-                    <h1>{counterTeam}</h1>
-                <ButtonCounter onClick={onDecrement} ref={btnDecrementRef}>-1</ButtonCounter> 
-            </div>
+            <button className="container-points" onClick={onIncrement}>
+                {counterTeam}
+            </button>
+            <ButtonDecrement onClick={onDecrement} ref={btnDecrementRef}>-1</ButtonDecrement> 
         </section>
     )
 }
