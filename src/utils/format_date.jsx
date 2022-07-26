1a16939c56c58f042e84ebcb9ec4f2c8affab8f0
NataliaFrancisca <nathifrancisca@gmail.com>
2022-07-26 15:48:31 -0300

    feat: format hour and minutes

--- src/utils/format_date.jsx
+++ src/utils/format_date.jsx
@@ -4,10 +4,14 @@ export const formatDate = (date) => {
     const [day, month, year] = [newDate.getDate(), newDate.getMonth(), newDate.getFullYear()];
     const [hour, minute] = [newDate.getHours(), newDate.getMinutes()];
 
-    return `${day} ${monthName(month)} ${year} - ${hour}:${minute}`;
+    return `${day} ${monthName(month)} ${year} - ${numberFormat(hour)}:${numberFormat(minute)}`;
 }
 
 const monthName = (month) => {
     const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     return months[month];
+}
+
+const numberFormat = (number) => {
+    return number < 9 ? `0${number}` : number;
 }
