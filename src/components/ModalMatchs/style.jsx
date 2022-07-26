7297966ad73d1fa43fd8fbffe3243d36edbee01b
NataliaFrancisca <nathifrancisca@gmail.com>
2022-07-26 10:26:03 -0300

    style: organize match style file

--- src/components/ModalMatchs/style.jsx
+++ src/components/ModalMatchs/style.jsx
@@ -2,16 +2,54 @@ import styled from "styled-components";
 import ModalMatchsComponent from ".";
 
 export const ModalMatchs = styled(ModalMatchsComponent)`
-    width: 60%;
-    height: 60%;
-
+    width: 50%;
+    min-height: 60%;
 
     position: absolute;
     transform: translate(-50%, -50%);
     left: 50%;
     top: 50%;
-
     z-index: 222;
 
-    background-color: orangered;
+
+    padding: 20px;
+
+    border-radius: 4px;
+    background-color: #e0eaea;
+
+    & header{
+        display: flex;
+        justify-content: space-between;
+    }
+
+    & h1{
+        font-size: 2em;
+    }
+
+    & button {
+        background-color: transparent;
+        cursor: pointer;
+
+        &:hover span{
+            color: #FFFFFF;
+        }
+
+        & span{
+            font-size: 36px;
+        }
+    }
+
+    & #list-of-matchs {
+        flex-direction: column;
+
+        height: 60vh;
+
+        display: block;
+        scroll-behavior: smooth;
+        overflow-y: auto;
+
+        border-radius: 4px;
+        margin: 10px 0;
+    }
 `
+
