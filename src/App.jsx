9913c792fe5206dad5b1d3a796a481e42ef536f0
NataliaFrancisca <nathifrancisca@gmail.com>
2022-07-26 15:46:15 -0300

    feat: import board buttons and create modal to explain the buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,52 @@
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
-import { BoardButtons, Button, BackgroundMask} from "./styles/elements";
+import { useState } from "react";
+
+import { Button, BackgroundMask, ModalButtonsFunction } from "./styles/elements";
 import { BoardCounter } from "./components/BoardCounter/style";
 import { ModalMatchs } from "./components/ModalMatchs/style";
-
-import { formatDate } from "./utils/format_date";
+import BoardButtons from "./components/BoardButtons";
 
 function App() {
 
-  const dispatch = useDispatch();
-
-  const counterHome = useSelector(state => state.reducerCountHome);
-  const counterVisitor = useSelector(state => state.reducerCountVisitor);
   const modalState = useSelector(state => state.reducerModal);
 
-  const onCleanScore = () => {
-    dispatch({type: 'cleanScore/visitor'});
-    dispatch({type: 'cleanScore/home'});
-  }
-
-  const onFinishMatch = () => {
-    dispatch({type: 'matchs/setMatchs', payload: onSaveMatch()})
-    onCleanScore();
-  }
-
-  const onOpenModal = () => dispatch({type: 'modal/toggle'});
-
-  const onSaveMatch = () => {
-    const date = new Date();
-    const formatedDate = formatDate(date);
-
-    const matchData = {
-      home: {name: 'marias', score: counterHome},
-      visitor: {name: 'joanas', score: counterVisitor},
-      date: formatedDate
-    };
-
-    return matchData;
-  }
+  const [modalButtonDetails, setModalButtonDetails] = useState(true);
 
   return  (
     <main className="App">
 
       <BackgroundMask filter={modalState} />
 
-      <BoardButtons>
-        <Button onClick={onCleanScore} backgroundColor={"#F32424"}>
-          <span className="material-symbols-outlined">mop</span>
-        </Button>
-
-        <Button onClick={onOpenModal} backgroundColor={"#0096FF"}>
-          <span className="material-symbols-outlined">visibility</span>
-        </Button>
-
-        <Button onClick={onFinishMatch} backgroundColor={"#3CCF4E"}>
-          <span className="material-symbols-outlined">check_small</span>
-        </Button>
-      </BoardButtons>
-
+      <BoardButtons />
       <BoardCounter />
+
+      {modalButtonDetails && 
+        <ModalButtonsFunction>
+          <div id="details-about-button">
+            <Button backgroundColor={"#F32424"}>
+              <span className="material-symbols-outlined">mop</span>
+            </Button>
+            <p>clean the score</p>
+          </div>
+        
+          <div id="details-about-button">
+            <Button backgroundColor={"#0096FF"}>
+              <span className="material-symbols-outlined">visibility</span>
+            </Button>
+            <p>show all the matches</p>
+          </div>
+
+          <div id="details-about-button">
+            <Button backgroundColor={"#3CCF4E"}>
+              <span className="material-symbols-outlined">check_small</span>
+            </Button>
+            <p>finish the match</p>
+          </div>
+
+          <button id="button-start" onClick={() => setModalButtonDetails(!modalButtonDetails)}>START</button>
+        </ModalButtonsFunction>
+      }
 
       {modalState && <ModalMatchs /> }
 
