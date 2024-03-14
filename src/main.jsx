09e5c726db4c0d8a628b9629d6cad866e07d6b46
Natalia Francisca <nathifrancisca@gmail.com>
2024-03-13 21:36:14 -0300

    feat: delete all the function related to react-redux

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import { GlobalStyle } from "./styles/global";
-import { Provider } from 'react-redux';
-
-import { createStore} from 'redux';
-import { rootReducer } from './reducers';
-
-const store = createStore(rootReducer);
+import { GlobalStyle } from './Global.style';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GlobalStyle />
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>
 )
