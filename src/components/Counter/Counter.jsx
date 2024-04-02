6106804ea8712cca228331c8fe211649bf5508a1
Natalia Francisca <nathifrancisca@gmail.com>
2024-04-01 21:24:23 -0300

    feat: renamed some function and add date-testid property

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useCounter } from "../../hooks/useCounter";
 import {CounterComponentStyled} from "./Counter.style";
 import Team from "../Team/Team";
@@ -5,13 +6,13 @@ import Menu from "../Menu/Menu";
 
 const Counter = ({onToggleModal}) => {
 
-    const { counterHomeTeam, counterVisitorTeam, onIncrement, onDecrement, onRestartCounter, onSaveMatchScore} = useCounter();
+    const { counterHomeTeam, counterVisitorTeam, onIncrement, onDecrement, onRestart, onSaveScore} = useCounter();
 
     return(
         <>
-            <Menu onToggleModal={onToggleModal} onRestartCounter={onRestartCounter} onSaveMatchScore={onSaveMatchScore}/>
+            <Menu onToggleModal={onToggleModal} onRestart={onRestart} onSaveScore={onSaveScore} />
 
-            <CounterComponentStyled>
+            <CounterComponentStyled data-testid={"counter-component"}>
                 <Team
                     name={'home'} 
                     counter={counterHomeTeam} 
