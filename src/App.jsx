c0c3ef081ed485e9697f68cf37133de557b0e619
Natalia Francisca <nathifrancisca@gmail.com>
2024-04-01 21:23:42 -0300

    feat: use the useModal custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
+import React from "react";
 import { BackgroundMask } from "./App.style";
 import Modal from "./components/Modal/Modal";
 import Counter from "./components/Counter/Counter";
 import { scoreService } from "./service/matchesService";
+import { useModal } from "./hooks/useModal";
+
 
 function App() {
 
-  const [toggleModal, setToggleModal] = useState(false);
-
-  const onToggleModal = () => setToggleModal((prev) => !prev);
+  const { toggleModal, onToggleModal } = useModal();
 
   scoreService.init();
 
